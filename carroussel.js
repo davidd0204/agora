84f83a8c623528215895c704c51e643591a47858
hug20003 <hugocresp@gmail.com>
2023-06-03 13:14:46 +0200

    Accueil

--- carroussel.js
+++ carroussel.js
@@ -6,43 +6,43 @@ $(document).ready(function() {
   $currentImg = $img.eq(i); // enfin, on cible l'image courante, qui possède l'index i (0 pour l'instant)
   $img.css("display", "none"); // on cache les images
   $currentImg.css("display", "block"); // on affiche seulement l'image courante
-  $carrousel.append('<div class="controls"><span class="prev">Precedent</span><span class="next">Suivant</span></div>');
+  $carrousel.append('<div class="controls"><span id="precedent"></span><span id="suivant"></span></div>');
 
-    $(document).on('click', '.next', function() { // image suivante
-        i++;
-        if (i > indexImg) {
-            i = 0;
-        }
-        $img.css('display', 'none');
-        $currentImg = $img.eq(i);
-        $currentImg.css('display', 'block');
-        });
-
-        $(document).on('click', '.prev', function() { // image précédente
-        i--;
-        if (i < 0) {
-            i = indexImg;
-        }
-        $img.css('display', 'none');
-        $currentImg = $img.eq(i);
-        $currentImg.css('display', 'block');
-    });
-        
-    function slideImg() {
-        setTimeout(function() {
-          if (i < indexImg) {
-            // si le compteur est inférieur au dernier index
-            i++; // on l'incrémente
-          } else {
-            // sinon, on le remet à 0 (première image)
-            i = 0;
-          }
-          $img.css("display", "none");
-          $currentImg = $img.eq(i);
-          $currentImg.css("display", "block");
-          slideImg(); // on oublie pas de relancer la fonction à la fin
-        }, 3000); // on définit l'intervalle à 4000 millisecondes (4s)
+  $(document).on('click', '#suivant', function() { // image suivante
+    i++;
+    if (i > indexImg) {
+      i = 0;
     }
+    $img.css('display', 'none');
+    $currentImg = $img.eq(i);
+    $currentImg.css('display', 'block');
+  });
+
+  $(document).on('click', '#precedent', function() { // image précédente
+    i--;
+    if (i < 0) {
+      i = indexImg;
+    }
+    $img.css('display', 'none');
+    $currentImg = $img.eq(i);
+    $currentImg.css('display', 'block');
+  });
+
+  function slideImg() {
+    setTimeout(function() {
+      if (i < indexImg) {
+        // si le compteur est inférieur au dernier index
+        i++; // on l'incrémente
+      } else {
+        // sinon, on le remet à 0 (première image)
+        i = 0;
+      }
+      $img.css("display", "none");
+      $currentImg = $img.eq(i);
+      $currentImg.css("display", "block");
+      slideImg(); // on oublie pas de relancer la fonction à la fin
+    }, 3000); // on définit l'intervalle à 4000 millisecondes (4s)
+  }
 
   slideImg(); // enfin, on lance la fonction une première fois
 });
