0c8be9afd11b1f17e23c37daff627d565734296f
davidd0204 <95612041+davidd0204@users.noreply.github.com>
2023-06-03 12:41:19 +0200

    suppr vendeur

--- fonction_admin.php
+++ fonction_admin.php
@@ -5,15 +5,28 @@
 	if($db_found)
 	{
 		$sql = "SELECT * FROM `compte client` WHERE `Type` = 1";
+		echo "<h1>Liste des vendeurs</h1>";
 		$result = mysqli_query($db_handle, $sql);
+		echo "<table border=\"1\">";
+		echo "<tr>";
+		echo "<th>" . "ID" . "</th>";
+		echo "<th>" . "Nom" . "</th>";
+		echo "<th>" . "Prénom" . "</th>";
+		echo "<th>" . "Adresse email" . "</th>";
+		echo "<th>" . "Supprimer Vendeur" . "</th>";
+		echo "</tr>";
 		while ($data = mysqli_fetch_assoc($result)) {
 			// code...
-			echo "Nom : " .$data['Nom'] . "<br>";
-			echo "Prénom : " .$data['Prénom'] . "<br>";
-			echo "Adresse : " .$data['Adresse'] . "<br>";
-			echo "Email : " .$data['Email'] . "<br>";
+			echo "<tr>";
+			echo "<td>" .$data['id'] . "</td>";
+			echo "<td>" .$data['Nom'] . "</td>";
+			echo "<td>" .$data['Prénom'] . "</td>";
+			echo "<td>" .$data['Email'] . "</td>";
+			echo "<td><button id='supprimer'>Supprimer</button></td>";
+			echo "</tr>";
 
 		}
+		echo "</table>";
 	}
 	else
 	{
