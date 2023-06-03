bbb38f480aa6125ee53148295572b67d315afc5a
Fripon210 <charlejeanja@gmail.com>
2023-06-03 15:54:33 +0200

    15h54

--- articles/parcourir.php
+++ articles/parcourir.php
@@ -122,49 +122,6 @@ button:hover {
     <tr>
       <th>Articles haut de gamme</th>
       <td>
-      <form action="afficher_articlesrare.php" method="POST">
-        <label for="article">Selectionnez un article :</label>
-        <select name="article" id="article">
-            <?php
-            // Connexion à la base de données
-            $database = "parcourir";
-            $db_handle = mysqli_connect('localhost', 'root', '', 'parcourir');
-            $db_found = mysqli_select_db($db_handle, $database);
-
-            if ($db_found) {
-                // Récupération des articles depuis la base de données
-                $sql = "SELECT * FROM articles_rare";
-                $result = mysqli_query($db_handle, $sql);
-
-                // Vérification si des résultats ont été trouvés
-                if (mysqli_num_rows($result) > 0) {
-                    while ($row = mysqli_fetch_assoc($result)) {
-                        $ID = $row["ID"];
-                        $Nom_article = $row["Nom_article"];
-                        ?>
-                        <option value="<?php echo $ID; ?>"><?php echo $Nom_article; ?></option>
-                        <?php
-                    }
-                } else {
-                    echo "<option disabled>Aucun article trouvé</option>";
-                }
-
-                // Fermer la connexion à la base de données
-                mysqli_close($db_handle);
-            } else {
-                echo "<option disabled>Erreur de connexion a la base de donnees</option>";
-            }
-            ?>
-        </select>
-        <br>
-        <button type="submit">Afficher les caracteristiques de l'article</button>
-    </form>
-      </td>
-      
-    </tr>
-    <tr>
-      <th>Articles rares</th>
-      <td>
       <form action="afficher_articlesHDG.php" method="POST">
         <label for="article">Selectionnez un article :</label>
         <select name="article" id="article">
@@ -203,8 +160,56 @@ button:hover {
         <button type="submit">Afficher les caracteristiques de l'article</button>
     </form>
       </td>
+      
+    </tr>
+    <tr>
+      <th>Articles rares</th>
+      <td>
+      <form action="afficher_articlesrare.php" method="POST">
+        <label for="article">Selectionnez un article :</label>
+        <select name="article" id="article">
+            <?php
+            // Connexion à la base de données
+            $database = "parcourir";
+            $db_handle = mysqli_connect('localhost', 'root', '', 'parcourir');
+            $db_found = mysqli_select_db($db_handle, $database);
+
+            if ($db_found) {
+                // Récupération des articles depuis la base de données
+                $sql = "SELECT * FROM articles_rare";
+                $result = mysqli_query($db_handle, $sql);
+
+                // Vérification si des résultats ont été trouvés
+                if (mysqli_num_rows($result) > 0) {
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $ID = $row["ID"];
+                        $Nom_article = $row["Nom_article"];
+                        ?>
+                        <option value="<?php echo $ID; ?>"><?php echo $Nom_article; ?></option>
+                        <?php
+                    }
+                } else {
+                    echo "<option disabled>Aucun article trouvé</option>";
+                }
+
+                // Fermer la connexion à la base de données
+                mysqli_close($db_handle);
+            } else {
+                echo "<option disabled>Erreur de connexion a la base de donnees</option>";
+            }
+            ?>
+        </select>
+        <br>
+        <button type="submit">Afficher les caracteristiques de l'article</button>
+    </form>
+      </td>
     </tr>
   </table>
 
 </body>
 </html>
+
+
+
+
+
